35e0eec2eda30cccbf0d7e95b0bedeeb76489e44
andresv2006 <andr.vargasm@duocuc.cl>
2026-09-03 20:56:30 -0400

    estructura inicial

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+const botonBienvenida = document.querySelector("#boton-bienvenida");
+
+botonBienvenida.addEventListener("click", () => {
+  alert("Construiremos el sitio de Veterinaria San Marcos paso a paso.");
+});
