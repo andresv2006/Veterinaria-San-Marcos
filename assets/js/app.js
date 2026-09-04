cc81840c1628a2784f889178aec0d7c5331ded14
andresv2006 <andr.vargasm@duocuc.cl>
2026-09-03 21:14:42 -0400

    agregar navegacion y pagina nosotros

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 const botonBienvenida = document.querySelector("#boton-bienvenida");
 
-botonBienvenida.addEventListener("click", () => {
-  alert("Bienvenido a Veterinaria San Marcos. Conoce nuestros servicios de consultas, vacunación y desparasitación en Rancagua.");
-});
+if (botonBienvenida) {
+  botonBienvenida.addEventListener("click", () => {
+    alert("Bienvenido a Veterinaria San Marcos. Conoce nuestros servicios de consultas, vacunación y desparasitación en Rancagua.");
+  });
+}
